52476714b4e19103ace7050b8221a80dc67333a8
Sudhakar <sudhakarmanoharan@outlook.com>
2019-08-28 23:50:41 +0530

    1st commit

---
+++ footer.php
@@ -0,0 +1,13 @@
+<!--footer-->
+	<footer>
+		
+			<!-- footer -->
+			<div class="footer-cpy text-center">
+			<div class="copyrightbottom">
+					<p> <span>&copy;&nbsp;</span>Infiniti Software Solutions <?php $current=date("Y"); print_r($current);?> </p>
+
+				</div>
+			</div>
+			<!-- //footer -->
+		</div>
+	</footer>
